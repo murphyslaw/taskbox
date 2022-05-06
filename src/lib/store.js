ae078a98da1878c4bdc2daf0f17ad066ed4a3403
Falk Köppe <falk.koeppe@gmail.com>
2022-05-07 00:21:06 +0200

    Bugfix: Keep the correct task ID type
    The issue was, that before changing to just `task.id`, I've used round
    braces instead of curly ones.

--- src/lib/store.js
+++ src/lib/store.js
@@ -29,7 +29,7 @@ export const fetchTasks = createAsyncThunk("todos/fetchTodos", async () => {
 
   const data = await response.json();
   const result = data.map((task) => ({
-    id: task.id,
+    id: `${task.id}`,
     title: task.title,
     state: task.completed ? "TASK_ARCHIVED" : "TASK_INBOX",
   }));
